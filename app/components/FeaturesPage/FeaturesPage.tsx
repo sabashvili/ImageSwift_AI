f00a65eee1022cf5d774ee34a14520ebba76afb9
Shota Sabashvili <s.sabashvili@ggtc.ge>
2023-12-04 12:06:19 +0400

    Create qualities section and also fix image scrolling

--- app/components/FeaturesPage/FeaturesPage.tsx
+++ app/components/FeaturesPage/FeaturesPage.tsx
@@ -11,40 +11,175 @@ import generateImage_7 from "../../../public/Images/GeneratedImagesExamples/gene
 import generateImage_8 from "../../../public/Images/GeneratedImagesExamples/generated-image-8.jpg";
 import generateImage_9 from "../../../public/Images/GeneratedImagesExamples/generated-image-9.jpg";
 import generateImage_10 from "../../../public/Images/GeneratedImagesExamples/generated-image-10.jpg";
+import qualities_icon_1 from "../../../public/Icons/qualities_icon_1.svg";
+import qualities_icon_2 from "../../../public/Icons/qualities_icon_2.svg";
+import qualities_icon_3 from "../../../public/Icons/qualities_icon_3.svg";
+import qualities_icon_4 from "../../../public/Icons/qualities_icon_4.svg";
 
 function FeaturesPage() {
   return (
     <section>
-      <div className={`${classes["scrolling-images-container"]} ${classes.ImageBox} ${classes["scrolling-time-for-first-line"]}`}>
-        <div>
-          <Image className={classes["generated-image"]} src={generateImage_1} alt="" />
-          <Image className={classes["generated-image"]} src={generateImage_2} alt="" />
-          <Image className={classes["generated-image"]} src={generateImage_3} alt="" />
-          <Image className={classes["generated-image"]} src={generateImage_4} alt="" />
-          <Image className={classes["generated-image"]} src={generateImage_5} alt="" />
+      <div className={classes["generated-images"]}>
+        <div
+          className={`${classes["scrolling-images-container"]} ${classes.ImageBox} ${classes["scrolling-time-for-first-line"]}`}
+        >
+          <div>
+            <Image
+              className={classes["generated-image"]}
+              src={generateImage_1}
+              alt=""
+            />
+            <Image
+              className={classes["generated-image"]}
+              src={generateImage_2}
+              alt=""
+            />
+            <Image
+              className={classes["generated-image"]}
+              src={generateImage_3}
+              alt=""
+            />
+            <Image
+              className={classes["generated-image"]}
+              src={generateImage_4}
+              alt=""
+            />
+            <Image
+              className={classes["generated-image"]}
+              src={generateImage_5}
+              alt=""
+            />
+          </div>
+          <div>
+            <Image
+              className={classes["generated-image"]}
+              src={generateImage_1}
+              alt=""
+            />
+            <Image
+              className={classes["generated-image"]}
+              src={generateImage_2}
+              alt=""
+            />
+            <Image
+              className={classes["generated-image"]}
+              src={generateImage_3}
+              alt=""
+            />
+            <Image
+              className={classes["generated-image"]}
+              src={generateImage_4}
+              alt=""
+            />
+            <Image
+              className={classes["generated-image"]}
+              src={generateImage_5}
+              alt=""
+            />
+          </div>
         </div>
-        <div>
-          <Image className={classes["generated-image"]} src={generateImage_1} alt="" />
-          <Image className={classes["generated-image"]} src={generateImage_2} alt="" />
-          <Image className={classes["generated-image"]} src={generateImage_3} alt="" />
-          <Image className={classes["generated-image"]} src={generateImage_4} alt="" />
-          <Image className={classes["generated-image"]} src={generateImage_5} alt="" />
+        <div
+          className={`${classes["scrolling-images-container"]} ${classes.ImageBox} ${classes["scrolling-time-for-second-line"]}`}
+        >
+          <div>
+            <Image
+              className={classes["generated-image"]}
+              src={generateImage_6}
+              alt=""
+            />
+            <Image
+              className={classes["generated-image"]}
+              src={generateImage_7}
+              alt=""
+            />
+            <Image
+              className={classes["generated-image"]}
+              src={generateImage_8}
+              alt=""
+            />
+            <Image
+              className={classes["generated-image"]}
+              src={generateImage_9}
+              alt=""
+            />
+            <Image
+              className={classes["generated-image"]}
+              src={generateImage_10}
+              alt=""
+            />
+          </div>
+          <div>
+            <Image
+              className={classes["generated-image"]}
+              src={generateImage_6}
+              alt=""
+            />
+            <Image
+              className={classes["generated-image"]}
+              src={generateImage_7}
+              alt=""
+            />
+            <Image
+              className={classes["generated-image"]}
+              src={generateImage_8}
+              alt=""
+            />
+            <Image
+              className={classes["generated-image"]}
+              src={generateImage_9}
+              alt=""
+            />
+            <Image
+              className={classes["generated-image"]}
+              src={generateImage_10}
+              alt=""
+            />
+          </div>
         </div>
       </div>
-      <div className={`${classes["scrolling-images-container"]} ${classes.ImageBox} ${classes["scrolling-time-for-second-line"]}`}>
-        <div>
-          <Image className={classes["generated-image"]} src={generateImage_6} alt="" />
-          <Image className={classes["generated-image"]} src={generateImage_7} alt="" />
-          <Image className={classes["generated-image"]} src={generateImage_8} alt="" />
-          <Image className={classes["generated-image"]} src={generateImage_9} alt="" />
-          <Image className={classes["generated-image"]} src={generateImage_10} alt="" />
-        </div>
-        <div>
-          <Image className={classes["generated-image"]} src={generateImage_6} alt="" />
-          <Image className={classes["generated-image"]} src={generateImage_7} alt="" />
-          <Image className={classes["generated-image"]} src={generateImage_8} alt="" />
-          <Image className={classes["generated-image"]} src={generateImage_9} alt="" />
-          <Image className={classes["generated-image"]} src={generateImage_10} alt="" />
+      <div className="sectionContainer">
+        <h1 className="main-header-section">Discover Our Pinnacle Qualities</h1>
+        <h2 className="secondary-header-section">
+          Essential Aspects That Define Us
+        </h2>
+        <div className={classes["qualities-container"]}>
+          <div className={classes["qualitie"]}>
+            <Image
+              className={classes["qualitie-icon"]}
+              src={qualities_icon_1}
+              alt="qualities icon"
+            />
+
+            <h1>Creativity</h1>
+            <p>This is where creativity reaches its zenith.</p>
+          </div>
+          <div className={classes["qualitie"]}>
+            <Image
+              className={classes["qualitie-icon"]}
+              src={qualities_icon_2}
+              alt="qualities icon"
+            />
+            <h1>Precision</h1>
+            <p>We have presented the most accurate algorithm.</p>
+          </div>
+          <div className={classes["qualitie"]}>
+            <Image
+              className={classes["qualitie-icon"]}
+              src={qualities_icon_3}
+              alt="qualities icon"
+            />
+            <h1>Speed</h1>
+            <p>Creating each image takes less than a minute.</p>
+          </div>
+          <div className={classes["qualitie"]}>
+            <Image
+              className={classes["qualitie-icon"]}
+              src={qualities_icon_4}
+              alt="qualities icon"
+            />
+            <h1>Artistry</h1>
+            <p>This is our most fundamental principle of work.</p>
+          </div>
         </div>
       </div>
     </section>
