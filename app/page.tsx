fcb455f8f3fce795c12c7ba9f541ed0890e581d5
sabashvili <sabashvili.shota@gmail.com>
2023-12-18 21:21:46 +0400

    Create Sign in page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import styles from "./page.module.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 import HomePage from "./components/HomePage/HomePage";
 import FeaturesPage from "./components/FeaturesPage/FeaturesPage";
 import PricingPage from "./components/PricingPage/PricingPage";
@@ -6,11 +8,15 @@ import AboutPage from "./components/AboutPage/AboutPage";
 
 export default function Home() {
   return (
-    <main>
-      <HomePage />
-      <FeaturesPage />
-      <PricingPage />
-      <AboutPage />
-    </main>
+    <>
+      <Header />
+      <main>
+        <HomePage />
+        <FeaturesPage />
+        <PricingPage />
+        <AboutPage />
+      </main>
+      <Footer />
+    </>
   );
 }
